5ad357a2812f7776e9611cf34debab24fb4f2c30
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-05-04 16:57:55 +0200

    [FINISH] ex01

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 17:45:30 by msharifi          #+#    #+#             */
+/*   Updated: 2023/05/04 16:55:37 by msharifi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hp;
+		int			_energy;
+		int			_attack;
+
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap &toCopy);
+		ClapTrap(std::string name, unsigned int hp, unsigned int energy, unsigned int attack);
+		~ClapTrap();
+
+		ClapTrap	&operator = (ClapTrap &toCopy);
+
+		void		status();
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 17:36:47 by msharifi          #+#    #+#             */
+/*   Updated: 2023/05/04 16:51:30 by msharifi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int	main()
+{
+	ClapTrap	Manuel("Manuel");
+
+	Manuel.attack("Naelle");
+	Manuel.takeDamage(5);
+	Manuel.beRepaired(2);
+	Manuel.status();
+
+	ScavTrap	Naelle("Naelle");
+
+	Naelle.attack("Manuel");
+	Naelle.takeDamage(2);
+	Naelle.beRepaired(1);
+	Naelle.guardGate();
+	Naelle.status();
+}
